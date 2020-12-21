156288e32d2dc3c9b31e03b5e8aa747eaa7ce33d
danielVFS <danielv759153@gmail.com>
2020-12-21 19:19:03 -0300

    feat: Header Component

--- App.js
+++ App.js
@@ -2,8 +2,15 @@ import "react-native-gesture-handler";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 
+import { COLORS } from "./constants";
+
 import Routes from "./routes";
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar style="dark" backgroundColor={COLORS.white} />
+      <Routes />
+    </>
+  );
 }
--- pages/Home.js
+++ pages/Home.js
@@ -1,12 +1,50 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import { COLORS, FONTS, SIZES, icons } from "../constants";
 
 const Home = () => {
+  function renderNavBar() {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          height: 70,
+          justifyContent: "space-between",
+          alignItems: "flex-end",
+          paddingHorizontal: SIZES.padding,
+          backgroundColor: COLORS.white,
+        }}
+      >
+        <TouchableOpacity
+          style={{ justifyContent: "center", width: 50 }}
+          onPress={() => alert("BACK")}
+        >
+          <Image
+            source={icons.back_arrow}
+            style={{ width: 30, height: 30, tintColor: COLORS.primary }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            justifyContent: "center",
+            alignItems: "flex-end",
+            width: 50,
+          }}
+          onPress={() => alert("info")}
+        >
+          <Image
+            source={icons.more}
+            style={{ width: 30, height: 30, tintColor: COLORS.primary }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ flex: 1, backgroundColor: COLORS.lightGray2 }}>
+      {renderNavBar()}
     </View>
   );
 };
