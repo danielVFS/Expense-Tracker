07154edf5299f202d40583f22f1eb7b7a35c97a3
danielVFS <danielv759153@gmail.com>
2020-12-23 15:52:44 -0300

    feat: PieChart

--- pages/Home.js
+++ pages/Home.js
@@ -458,10 +458,38 @@ const Home = () => {
   function renderChart() {
     let chartData = processCategoryDataToDisplay();
     let colorScales = chartData.map((item) => item.color);
+    let totalExpenseCount = chartData.reduce(
+      (a, b) => a + (b.expenseCount || 0),
+      0
+    );
 
     return (
-      <View>
-        <VictoryPie data={chartData} colorScale={colorScales} />
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <VictoryPie
+          data={chartData}
+          colorScale={colorScales}
+          labels={({ datum }) => `${datum.y}`}
+          radius={SIZES.width * 0.4 - 10}
+          innerRadius={70}
+          labelRadius={({ innerRadius }) =>
+            (SIZES.width * 0.4 + innerRadius) / 2.5
+          }
+          style={{
+            labels: { fill: COLORS.white, ...FONTS.body3 },
+            parent: {
+              ...styles.shadow,
+            },
+          }}
+          width={SIZES.width * 0.8}
+          height={SIZES.width * 0.8}
+        />
+
+        <View style={{ position: "absolute" }}>
+          <Text style={{ ...FONTS.h1, textAlign: "center" }}>
+            {totalExpenseCount}
+          </Text>
+          <Text style={{ ...FONTS.body4, textAlign: "center" }}>Expenses</Text>
+        </View>
       </View>
     );
   }
